f8c8e027ff7e43598ab02cb864da1e587ab70778
NoSocksForYou <kaappo.raivio@gmail.com>
2018-04-25 11:43:33 +0300

    asd

--- shakki.py
+++ shakki.py
@@ -1,37 +1,6 @@
-class SuhteellinenKoordinaatti:
-    def __init__(self, delta_x, delta_y):
-        self.x = delta_x
-        self.y = delta_y
-
-    def __iter__(self):
-        yield self.x
-        yield self.y
-
-    @property
-    def x(self):
-        return x
-
-    @x.setter
-    def x(self, val):
-        if val > 8:
-            self.x = 8
-        elif val < -8:
-            self.x = -8
-        else:
-            self.x = val
-
-    @property
-    def y(self):
-        return y
-
-    @y.setter
-    def y(self, val):
-        if val > 8:
-            self.y = 8
-        elif val < -8:
-            self.y = -8
-        else:
-            self.y = val
+from errors import *
+from koordinaatti import AbsoluuttinenKoordinaatti, SuhteellinenKoordinaatti
+from shakkinappula import Shakkinappula
 
 
 class Shakkiruutu:
@@ -39,7 +8,10 @@ class Shakkiruutu:
     __id = 0
 
     def __init__(self, koordinaatti, nappula):
-        self.__pos_x, self.__pos_y = pos_y = *koordinaatti
+        if isinstance(koordinaatti, AbsoluuttinenKoordinaatti):
+            self.__pos_x, self.__pos_y = koordinaatti
+        else:
+            raise TypeError("Invalid type for the position!")
 
         self.nappula = nappula
 
@@ -72,56 +44,10 @@ class Shakkiruutu:
         print("Ei onnistu pos_x:n asetus")
 
 
-class AbsoluuttinenKoordinaatti:
-    def __init__(self, pos_x, pos_y):
-        self.x = pos_x
-        self.y = pos_y
-
-    def __iter__(self):
-        yield self.x
-        yield self.y
-
-    @property
-    def x(self):
-        return x
-
-    @x.setter
-    def x(self, val):
-        if val > 8:
-            self.x = 8
-        elif val < -8:
-            self.x = -8
-        else:
-            self.x = val
-
-    @property
-    def y(self):
-        return y
-
-    @y.setter
-    def y(self, val):
-        if val > 8:
-            self.y = 8
-        elif val < -8:
-            self.y = -8
-        else:
-            self.y = val
-
-
 class Shakkilauta:
     def __init__(self):
-        self.ruudut = [[Shakkiruutu(x, y, None) for x in range(9)] for y in range(9)]
-
-
-class Shakkinappula:
-    """Älä instantioi tästä!!!"""
-
-    def __init__(self, allowed_squares, sijainti):
-        self.relative_allowed_squares = allowed_squares
-        self.pos_x, self.pos_y = *sijainti
-
-    def __iter__(self):
-
+        self.ruudut = [[Shakkiruutu(AbsoluuttinenKoordinaatti(x, y), None)
+                        for x in range(9)] for y in range(9)]
 
 
 class Sotilas(Shakkinappula):
@@ -151,4 +77,6 @@ class Kuningatar(Shakkinappula):
 
 
 a = Shakkilauta()
-print(a.ruudut)
+# print(a.ruudut)
+
+print(AbsoluuttinenKoordinaatti(5, 5) + SuhteellinenKoordinaatti(1, 2))
